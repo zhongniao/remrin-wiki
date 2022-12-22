8a8781dddda4cc57b5fafc09c0bcfc59b7e889f5
Remrin <228827120@qq.com>
2022-12-22 15:41:54 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/com/remrin/wiki/controller/TestController.java
+++ src/main/java/com/remrin/wiki/controller/TestController.java
@@ -1,12 +1,18 @@
 package com.remrin.wiki.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @RequestMapping("/hello")
+    @GetMapping ("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @PostMapping  ("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post, " + name;
     }
 }
