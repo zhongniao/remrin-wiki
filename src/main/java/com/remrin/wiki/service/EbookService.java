cff96fac39dd3516c9fd881df514c17a0b5c9fe5
zhongniao <jian4mang2@gmail.com>
2022-12-29 16:57:49 +0800

    3-8 制作CopyUtil封装BeanUtils

--- src/main/java/com/remrin/wiki/service/EbookService.java
+++ src/main/java/com/remrin/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.remrin.wiki.domain.EbookExample;
 import com.remrin.wiki.mapper.EbookMapper;
 import com.remrin.wiki.req.EbookReq;
 import com.remrin.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.remrin.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,13 +22,16 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+//            EbookResp ebookResp = new EbookResp();
+//            BeanUtils.copyProperties(ebook, ebookResp);
+//            respList.add(ebookResp);
+//        }
 
-        return respList;
+        //列表复制
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+
+        return list;
     }
 }
