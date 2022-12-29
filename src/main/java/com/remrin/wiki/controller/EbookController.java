9b1c4ec2b142c4e0ca34735a3024975bce3ca275
zhongniao <jian4mang2@gmail.com>
2022-12-29 14:48:53 +0800

    3-6 增加通用的返回类CommonResp，以后所有后端接口的返回值全部都是CommonResp类型

--- src/main/java/com/remrin/wiki/controller/EbookController.java
+++ src/main/java/com/remrin/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.remrin.wiki.controller;
 
 import com.remrin.wiki.domain.Ebook;
+import com.remrin.wiki.resp.CommonResp;
 import com.remrin.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping ("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
