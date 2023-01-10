8d27f4f053c6ed9fd4df884de2c2f093157866f8
Remrin <228827120@qq.com>
2023-01-10 16:45:02 +0800

    6-4 集成PageHelper插件，将mapper日志级别改为trace

--- src/main/java/com/remrin/wiki/service/EbookService.java
+++ src/main/java/com/remrin/wiki/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.remrin.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.remrin.wiki.domain.Ebook;
 import com.remrin.wiki.domain.EbookExample;
 import com.remrin.wiki.mapper.EbookMapper;
 import com.remrin.wiki.req.EbookReq;
 import com.remrin.wiki.resp.EbookResp;
 import com.remrin.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -14,6 +18,8 @@ import java.util.List;
 
 @Service
 public class EbookService {
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
     @Resource
     private EbookMapper ebookMapper;
 
@@ -23,7 +29,13 @@ public class EbookService {
         if (!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
+
+        PageHelper.startPage(1, 3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
 
         // List<EbookResp> respList = new ArrayList<>();
         // for (Ebook ebook : ebookList) {
