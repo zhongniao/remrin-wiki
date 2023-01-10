26b333779b99db3d3e720a8c556ceb5f179b660e
Remrin <228827120@qq.com>
2023-01-10 18:39:52 +0800

    6-7 模态框显示电子书表单，编辑某一行时可以将该行数据显示到表单

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -22,7 +22,7 @@
           </template>
           <template v-else-if="column.key === 'action'">
             <a-space size="small">
-              <a-button type="primary" @click="edit">
+              <a-button type="primary" @click="edit(record)">
                 编辑
               </a-button>
               <a-button type="danger">
@@ -41,7 +41,25 @@
       :confirm-loading="modalLoading"
       @ok="handleModalOk"
   >
-    <p>test</p>
+    <p>
+      <a-form :model="ebook" :label-col="{ span: 6 }">
+        <a-form-item label="封面">
+          <a-input v-model:value="ebook.cover" />
+        </a-form-item>
+        <a-form-item label="名称">
+          <a-input v-model:value="ebook.name" />
+        </a-form-item>
+        <a-form-item label="分类一">
+          <a-input v-model:value="ebook.category1Id" />
+        </a-form-item>
+        <a-form-item label="分类二">
+          <a-input v-model:value="ebook.category2Id" />
+        </a-form-item>
+        <a-form-item label="描述">
+          <a-input v-model:value="ebook.desc" type="text" />
+        </a-form-item>
+      </a-form>
+    </p>
   </a-modal>
 </template>
 
@@ -136,6 +154,7 @@ export default defineComponent({
     }
 
     // -------- 表单 --------
+    const ebook = ref({});
     // const modalText = ref<string>('Content of the modal');
     const modalVisible = ref<boolean>(false);
     const modalLoading = ref<boolean>(false);
@@ -156,8 +175,9 @@ export default defineComponent({
     /**
      * 编辑
      */
-    const edit = () => {
+    const edit = (record: any) => {
       modalVisible.value = true
+      ebook.value = record
     }
 
     onMounted(() => {
@@ -173,7 +193,10 @@ export default defineComponent({
       columns,
       loading,
       handleTableChange,
+
       edit,
+
+      ebook,
       modalVisible,
       modalLoading,
       handleModalOk
