19b08bdc4c73eb4be8686a33f011d733eb0c33ab
Remrin <228827120@qq.com>
2023-01-10 17:12:52 +0800

    6-5 封装分页请求参数PageReq和返回参数PageResp

--- src/main/java/com/remrin/wiki/controller/EbookController.java
+++ src/main/java/com/remrin/wiki/controller/EbookController.java
@@ -3,13 +3,13 @@ package com.remrin.wiki.controller;
 import com.remrin.wiki.req.EbookReq;
 import com.remrin.wiki.resp.CommonResp;
 import com.remrin.wiki.resp.EbookResp;
+import com.remrin.wiki.resp.PageResp;
 import com.remrin.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @RequestMapping("/ebook")
@@ -19,8 +19,8 @@ public class EbookController {
 
     @GetMapping ("/list")
     public CommonResp list(EbookReq req) {
-        CommonResp<List<EbookResp>> resp = new CommonResp<>();
-        List<EbookResp> list = ebookService.list(req);
+        CommonResp<PageResp<EbookResp>> resp = new CommonResp<>();
+        PageResp<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
