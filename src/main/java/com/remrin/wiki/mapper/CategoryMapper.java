5fc3e55d3138848625afb22404624672910f7e7c
zhongniao <jian4mang2@gmail.com>
2023-01-11 15:19:53 +0800

    7-2 分类表设计与持久层代码生成 基本无限极树型表结构设计：id+parent

---
+++ src/main/java/com/remrin/wiki/mapper/CategoryMapper.java
@@ -0,0 +1,31 @@
+package com.remrin.wiki.mapper;
+
+import com.remrin.wiki.domain.Category;
+import com.remrin.wiki.domain.CategoryExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface CategoryMapper {
+    long countByExample(CategoryExample example);
+
+    int deleteByExample(CategoryExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Category row);
+
+    int insertSelective(Category row);
+
+    List<Category> selectByExample(CategoryExample example);
+
+    Category selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("row") Category row, @Param("example") CategoryExample example);
+
+    int updateByExample(@Param("row") Category row, @Param("example") CategoryExample example);
+
+    int updateByPrimaryKeySelective(Category row);
+
+    int updateByPrimaryKey(Category row);
+}
