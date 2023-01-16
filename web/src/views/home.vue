9802550b3f55ac5815b3ad172888ec9623f574a6
Remrin <228827120@qq.com>
2023-01-16 17:50:07 +0800

    7-8 默认显示欢迎页面，点击欢迎菜单时，显示欢迎页面，点击分类菜单时，显示电子书，只有二级分类才能触发click事件

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -9,10 +9,8 @@
           @click="handleClick"
       >
         <a-menu-item key="welcome">
-          <router-link :to="'/'">
-            <MainOutlined />
-            <span>欢迎</span>
-          </router-link>
+          <MainOutlined />
+          <span>欢迎</span>
         </a-menu-item>
         <a-sub-menu v-for="item in level1" :key="item.id">
           <template #title>
@@ -27,7 +25,10 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
+      <div class="welcome" v-show="isShowWelcome">
+        <h1>欢迎使用REMRIN知识库</h1>
+      </div>
+      <a-list v-show="!isShowWelcome" item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
         <template #renderItem="{ item }">
           <a-list-item key="item.name">
             <template #actions>
@@ -102,8 +103,16 @@ export default defineComponent({
       })
     }
 
-    const handleClick = () => {
-      console.log("menu click")
+    const isShowWelcome = ref(true)
+
+    const handleClick = (value: any) => {
+      // console.log("menu click")
+      // if (value.key === 'welcome') {
+      //   isShowWelcome.value = true
+      // } else {
+      //   isShowWelcome.value = false
+      // }
+      isShowWelcome.value = value.key === 'welcome';
     }
 
     const pagination = {
@@ -140,7 +149,9 @@ export default defineComponent({
       pagination,
       actions,
       handleClick,
-      level1
+      level1,
+
+      isShowWelcome
     }
   }
 });
